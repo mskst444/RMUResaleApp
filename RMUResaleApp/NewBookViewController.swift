ca0c73c6c6ace2636b2cbb67b46edb49f6af49e0
Jacob Wright <jrwst132@whe-133-m01.local>
2019-04-17 18:47:55 -0400

    forget it

---
+++ RMUResaleApp/NewBookViewController.swift
@@ -0,0 +1,45 @@
+//
+//  NewBookViewController.swift
+//  RMUResaleApp
+//
+//  Created by Jacob Wright on 4/17/19.
+//  Copyright © 2019 Mitchell Keller. All rights reserved.
+//
+
+import UIKit
+
+class NewBookViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var newTitle: UITextField!
+    @IBOutlet weak var newPrice: UITextField!
+    @IBOutlet weak var newISBN: UITextField!
+    @IBOutlet weak var newAuthor: UITextField!
+    
+    @IBOutlet weak var displayName: UILabel!
+    @IBOutlet weak var displayEmail: UILabel!
+    
+  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.newTitle.delegate = self
+        self.newPrice.delegate = self
+        self.newISBN.delegate  = self
+        self.newAuthor.delegate = self
+    }
+    
+    let 
+    
+    
+    
+    @IBAction func newListing(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
+    
+    
+}
