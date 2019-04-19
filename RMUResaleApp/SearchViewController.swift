9d03a932747986242667b0f1c1c0c76b99bef39e
Mitchell Keller <mskst444@whe-144-m03.local>
2019-04-19 17:17:10 -0400

    updated search for title/author

--- RMUResaleApp/SearchViewController.swift
+++ RMUResaleApp/SearchViewController.swift
@@ -115,6 +115,11 @@ class SearchViewController: UIViewController, UITextFieldDelegate {
     //Function to search for a book by Title and Author
     func mySearchRequest(_ title: String, _ author: String)
     {
+        SearchResult.author = []
+        SearchResult.isbn = []
+        SearchResult.price = []
+        SearchResult.sellerUsername = []
+        SearchResult.title = []
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else
         {
             return
@@ -138,17 +143,18 @@ class SearchViewController: UIViewController, UITextFieldDelegate {
                  If there is no identical username already created, this loop will not even run once
                  */
                 
-                print("Author: \(book.value(forKey: "author")!)")
-                print("ISBN: \(book.value(forKey: "isbn")!)")
-                print("Price: \(book.value(forKey: "price")!)")
-                print("Username: \(book.value(forKey: "sellerUsername")!)")
-                print("Title: \(book.value(forKey: "title")!)")
+                SearchResult.author.append("\(book.value(forKey: "author")!)")
+                SearchResult.isbn.append("\(book.value(forKey: "isbn")!)")
+                SearchResult.price.append(book.value(forKey: "price")! as! Float)
+                SearchResult.sellerUsername.append("\(book.value(forKey: "sellerUsername")!)")
+                SearchResult.title.append("\(book.value(forKey: "title")!)")
             }
             
         } catch let error{
             print(error)
             return
         }
+        print(SearchResult.author)
         return
     }
 
