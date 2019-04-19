7764f9b5001bda69d8503e0c479aad3582ef7d10
Jacob Wright <jrwst132@whe-144-m02.local>
2019-04-18 20:14:40 -0400

    creating segue functionality in account overview table

--- RMUResaleApp/AccountOverviewTableViewController.swift
+++ RMUResaleApp/AccountOverviewTableViewController.swift
@@ -84,6 +84,20 @@ class AccountOverviewTableViewController: UITableViewController {
         return cell
     }
 
+    override func tableView( _ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+        
+        let listingViewController = storyboard?.instantiateViewController(withIdentifier: "ListingViewController") as! ListingViewController
+        
+        let listData = cellData[indexPath.row]
+        listingViewController.listingTitle.text = listData.value(forKeyPath: "title") as? String
+        listingViewController.listingPrice.text = listData.value(forKeyPath: "price") as? String
+        listingViewController.listingSeller.text = listData.value(forKeyPath: "seller") as? String
+        
+        navigationController?.pushViewController(listingViewController, animated: true)
+    }
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
