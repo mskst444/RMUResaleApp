41209ae2b7ca22d63d80370828cadcba6a0e54a2
Jacob Wright <jrwst132@whe-144-m02.local>
2019-04-18 20:38:25 -0400

    remodeled home screen

--- RMUResaleApp/AccountOverviewTableViewController.swift
+++ RMUResaleApp/AccountOverviewTableViewController.swift
@@ -98,6 +98,8 @@ class AccountOverviewTableViewController: UITableViewController {
     }
     
     
+    
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
